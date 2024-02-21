9b0be66babb36694ccf4b0af0a73d0c7f4b0892a
Luiz Felipe Cordeiro <114431500+Felipex7z@users.noreply.github.com>
2024-02-20 23:12:41 -0300

    cmt

---
+++ datas/indexx.php
@@ -0,0 +1,12 @@
+<?php
+
+$data = date('d/m/y'); // day/ month / year 
+
+echo $data . "<br>";
+
+$data2 = date('D/M/Y'); // abreviado com letras
+echo $data2; 
+echo "<br>";
+
+$data3 = date('l, F - Y'); // date completed
+echo $data3;
