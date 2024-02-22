0b22484b3c1b84f65a50fa6e5edf14fb0af1ad28
Luiz Felipe Cordeiro <114431500+Felipex7z@users.noreply.github.com>
2024-02-22 01:29:30 -0300

    cmt

---
+++ banco-de-dados/comando.sql
@@ -0,0 +1,5 @@
+para criar o banco de dados se usa 
+CREATE DATABASE nomedobanco;
+
+para deletar o banco de dados se usa 
+DROP DATABASE nomedobanco;
---
+++ banco-de-dados/tipos.de.dados.sql
@@ -0,0 +1,9 @@
+VARCHAR: texto de 0 a 65535 caracteres;
+
+TEXT: texto com no máximo 65535 bytes;
+
+INT: números inteiros;
+
+BIGINT: números inteiros com maior proporção que o INT;
+
+DATE: data no formato YYYY-MM-DD
