dab038cf6684e0515df04914a973731e4207ab8c
Luiz Felipe Cordeiro <114431500+Felipex7z@users.noreply.github.com>
2024-02-23 19:54:48 -0300

    cmt

--- banco-de-dados/selecionando/index.php
+++ banco-de-dados/selecionando/index.php
@@ -6,3 +6,15 @@ $db = "cursophp";
 
 $conexao = new mysqli("localhost", "root", "", "cursophp");
 // selecionando dados
+
+$q = "SELECT * FROM pessoas";
+$resultado = $conexao->query($q);
+$conexao->close();
+
+// um resultado
+$item = $resultado->fetch_assoc();
+
+//todos os resultados
+$itens = $resultado->fetch_all();
+
+print_r($itens);
